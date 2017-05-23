abca1a40cc94a19e2e8624fe64a40a6c36dc6401
Chadrick Mahaffey <chadrickm@gmail.com>
2017-05-22 21:27:01 -0400

    testing the Bittrex API

---
+++ BittrexApiCaller/Program.cs
@@ -0,0 +1,32 @@
+﻿using Bittrex;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BittrexApiCaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exchangeContext = new ExchangeContext();
+            exchangeContext.ApiKey = "";
+            exchangeContext.Secret = "";
+            exchangeContext.QuoteCurrency = "BTC";
+            exchangeContext.Simulate = true;
+
+            var exchange = new Exchange();
+            exchange.Initialise(exchangeContext);
+
+            var results = exchange.GetOrderBook("PIVX", OrderBookType.Both, 50);
+            foreach (var result in results.buy)
+            {
+                Console.WriteLine(result.Rate + ": " + result.Quantity);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ console/Program.cs
@@ -0,0 +1,25 @@
+﻿using Bittrex;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exchangeContext = new Bittrex.ExchangeContext();
+            exchangeContext.ApiKey = "d30ac07ad78143a39b1fd1073c75a5d9";
+            exchangeContext.Secret = "7fee6a6d1e6c4113b6522c74070cfb99";
+            exchangeContext.QuoteCurrency = "BTC";
+            exchangeContext.Simulate = true;
+
+            var exchange = new Bittrex.Exchange();
+            exchange.Initialise(exchangeContext);
+
+            GetOpenOrdersResponse openOrders = exchange.GetOpenOrders("BTC-PIVX");
+            foreach (var openOrder in openOrders)
+            {
+
+            }
+        }
+    }
+}
